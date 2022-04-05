8367246b45849d96f56b2d5d543358f14bacc6ce
Avital <avri223@walla.com>
2022-04-05 15:29:41 +0300

    T5 add login via OKTA

--- src/Server/index.ts
+++ src/Server/index.ts
@@ -1,42 +1,30 @@
 import * as express from 'express'
 import * as path from "path";
-import * as http from "http";
-const app = express()
+import * as dotenv from "dotenv";
+import * as sessionAuth from "../middleware/sessionAuth";
+import * as routes from "../routes";
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
+const app = express();
 
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname.split("dist")[0], "dist/views" ) );
 app.set( "view engine", "ejs" );
 
-app.get('/', (req, res) => {
-    res.render( "index" );
-});
+// Configure session auth
+sessionAuth.register( app );
 
-app.use((req, res, next) => {
-    let newPath = req.path;
-    // remove any query string
-    const index = newPath.indexOf("?");
-    if (index >= 0) {
-        newPath = newPath.slice(0, index);
-    }
+// Configure routes
+routes.register( app );
 
-    // remove leading /
-    newPath = newPath.split("/")[1];
-
-    // let res.render() tell us whether the file exists or not
-    res.render(newPath,  (err: any, resp: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; send: (arg0: any) => void; }) => {
-        if (err) {
-            next()
-        } else {
-            res.send(resp);
-        }
-    });
-});
-
-app.use((req, res) =>{
-    res.status(404).send("Sorry, that page doesn't exist. Have a nice day :)");
-});
-// your express configuration here
-const httpServer = http.createServer(app);
-
-// For https
-httpServer.listen(8080);
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
