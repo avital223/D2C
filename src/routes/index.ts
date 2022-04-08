4a581a018222bd16bdc33fa72cbc0da6e1b63f73
Avital <avri223@walla.com>
2022-04-08 12:27:10 +0300

    T5 Add also navigation bar

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,24 +3,22 @@ import * as express from "express";
 export const register = ( app: express.Application ) => {
     const oidc = app.locals.oidc;
 
-    // define a route handler for the default home page
     app.get( "/", ( req: any, res ) => {
-        res.render( "index" );
+        const user = req.userContext ? req.userContext.userinfo : null;
+        res.render( "index", { isAuthenticated: req.isAuthenticated(), user } ); // will be the inro page of the web
     } );
 
-    // define a secure route handler for the login page that redirects to /guitars
     app.get( "/login", oidc.ensureAuthenticated(), ( req, res ) => {
-        res.redirect( "/questionare" );
+        res.redirect( "/questionare" ); // change to main page one is created!
     } );
 
-    // define a route to handle logout
     app.get( "/logout", ( req: any, res ) => {
         req.logout();
         res.redirect( "/" );
     } );
 
-    // define a secure route handler for the guitars page
     app.get( "/questionare", oidc.ensureAuthenticated(), ( req: any, res ) => {
-        res.render( "questionare" );
+        const user = req.userContext ? req.userContext.userinfo : null;
+        res.render( "questionare", { isAuthenticated: req.isAuthenticated(), user } );
     } );
 };
