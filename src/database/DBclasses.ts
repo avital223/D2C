d14dea0517e5e0cd2be622ece1849a245ee00aef
Avital <avri223@walla.com>
2022-04-19 17:50:49 +0300

    change fike name

---
+++ src/database/DBclasses.ts
@@ -0,0 +1,5 @@
+import { ObjectId } from "mongodb";
+
+export default class Questionare {
+    constructor(public name: string, public questions: string[], public category: number[], public answers: string[][], public id?: ObjectId) {}
+}
